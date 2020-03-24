f7e48ef3431f424cac036ad01534eaa2bd393b5c
HaQadosch Berraka <haqadosch@pm.me>
2020-03-24 23:57:49 +0000

    handle create user calls with empty payload

--- spec/cucumber/steps/index.js
+++ spec/cucumber/steps/index.js
@@ -1,31 +1,47 @@
+const superagent = require('superagent')
+const assert = require('assert')
 const { When, Then, Given } = require('cucumber')
 
 // Given('a POST request to /user created with a generic empty payload', callback => {
 //   callback(null, 'pending')
 // })
+const given = {}
 
-Given('a POST request to \/user created with a generic empty payload', function () {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending'
+Given('a POST request to /user created with a generic empty payload', function () {
+  // By default superagent creates an empty payload when none is given.
+  given.request = superagent('POST', `${process.env.SERVER_HOSTNAME}:${process.env.SERVER_PORT}/user`)
 })
 
-When('the client sends the request', function () {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending'
+When('the client sends the request', function (callback) {
+  given.request
+    .then(response => {
+      given.response = response.res
+      callback()
+    })
+    .catch(errResponse => {
+      given.response = errResponse.response
+      callback()
+    })
 })
 
 Then('our API should respond with a {int} HTTP status code', function (int) {
-  // Then('our API should respond with a {float} HTTP status code', function (float) {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending'
+  assert.strictEqual(given.response.statusCode, int)
 })
 
 Then('the payload of the response should be a JSON object', function () {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending'
+  const { text, headers } = given.response
+  const contentType = headers && (headers['Content-Type'] || headers['content-type'])
+  if (!contentType || !contentType.includes('application/json')) {
+    throw new Error('Response not of content application/json')
+  }
+
+  try {
+    given.payload = JSON.parse(text)
+  } catch (parseError) {
+    throw new Error('Response is not a valid JSON object')
+  }
 })
 
 Then('contains a message property which says {string}', function (string) {
-  // Write code here that turns the phrase above into concrete actions
-  return 'pending'
+  assert.strictEqual(given.payload.message, string)
 })
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const http = require('http')
 
 const requestHandler = (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Hello World!')
+  if (req.method === 'POST' && req.url === '/user') {
+    res.writeHead(400, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({
+      message: 'Payload should not be empty'
+    }))
+  } else {
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+    res.end('Hello World!')
+  }
 }
 
 const server = http.createServer(requestHandler)
-server.listen(8000)
+server.listen(8080)
